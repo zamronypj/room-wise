b186a8706e56b45d5f25b0e653c6319823e60757
Zamrony P. Juhara <zamronypj@yahoo.com>
2024-12-09 05:54:01 +0700

    change logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 		<div className="bg-sky-500 py-6">
 			<div className="container mx-auto flex justify-between">
 				<span className="text-4xl font-hotel font-bold tracking-tight text-white">
-					<Link to="/">Room Wise</Link>
+					<Link to="/">Go Gili</Link>
 				</span>
 
 				<span className="flex space-x-2">
